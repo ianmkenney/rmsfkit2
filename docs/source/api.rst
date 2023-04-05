1af0e0c2b433cf527cc25a87ad662e680720321e
Ian Kenney <ianmichaelkenney@gmail.com>
2023-04-05 14:16:48 -0700

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ docs/source/api.rst
@@ -0,0 +1,7 @@
+API Documentation
+=================
+
+.. autosummary::
+   :toctree: autosummary
+
+   rmsfkit2.canvas
---
+++ rmsfkit/__init__.py
@@ -0,0 +1,14 @@
+"""
+rmsfkit
+A short description of the project.
+"""
+
+# Add imports here
+from .rmsfkit import canvas
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
